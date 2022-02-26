90d6a00ca3c8e23350c5d5d2fd3a2fb03e1fc3e7
baris.oztas@metu.edu.tr <Aklandsis1234?!>
2022-02-26 19:28:35 +0300

    calculate zonal stats

--- era5_statistics.py
+++ era5_statistics.py
@@ -2,19 +2,22 @@ import os
 import datetime
 import pathlib
 
-import xarray
+import geopandas as gpd
+import pandas as pd
+import xarray as xr
 from rasterstats import zonal_stats
 import rasterio as rio
-import fiona
+
 
 
 class Era5Extraction(object):
     def __init__(self):
         # Define folder where grids are reside in
         self.start_time = datetime.datetime.now()
+        self.PARAMETER = 't2m'
         self.input_folder_era5 = r"/home/hsaf/ponderful/ERA5/2m_temperature/daily_era5"
-        self.output_folder_zonal_stats = r"/home/hsaf/ponderful/ERA5/ZonalStats"
-        self.grid_folder = r"/home/hsaf/PycharmProjects/cmip6_downscaling/results/elevation_median/"
+        self.output_folder_zonal_stats = r"./results/ZonalStats"
+        self.grid_folder = r"./results/elevation_median/"
         self.era5_path = None
         self.era5_ds = None
         self.grid_list = []
@@ -31,25 +34,25 @@ class Era5Extraction(object):
         print("Grids of CMIP6 is found!")
 
     def read_era5(self):
-        self.era5_ds = xarray.load_dataset(self.era5_path)
+        self.era5_ds = xr.open_dataset(self.era5_path)
+        self.era5_ds = self.era5_ds[self.PARAMETER]
+        self.era5_da = self.era5_ds[:,:,:]
         print("ERA5 is read!")
 
     def calculate_zonal_stats(self, grid_polygon):
-
+        affine = rio.open(self.era5_path).transform
+        shp_df = gpd.read_file(grid_polygon)
         # loop into geometries of grids of CMIP6
-        with fiona.open(grid_polygon) as src:
-
             # get the days of era5
-            all_days = self.era5_ds['time'].values
-
+        all_days = self.era5_ds['time']
             # loop into each day and calculate statistics
-            for day in all_days:
-                affine = rio.open(self.era5_path).transform
-                era5_ds_day = self.era5_ds.sel(time=day).values
-
-                era5_stats = zonal_stats(src, era5_ds_day, affine=affine,
-                                         stats=['min', 'max', 'median', 'mean'])
-                print(era5_stats)
+        for day in range(len(all_days)):
+            era5_da = self.era5_ds[day,:,:]
+            era5_da = era5_da.values
+            era5_stats = zonal_stats(shp_df.geometry, era5_da,
+                                         affine=affine, stats=['min', 'max', 'median', 'mean', 'count'])
+            era5_stats_pd = pd.DataFrame.from_records(era5_stats)
+            print(era5_stats)
 
     def report_output(self):
         pass
