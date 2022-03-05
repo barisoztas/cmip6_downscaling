efb024dcd71fcdd16e01c04557499a108e35a2a2
Barış Öztaş <baris.oztas@metu.edu.tr>
2022-03-05 23:23:51 +0300

    Update cmip6_extracting.py

--- cmip6_extracting.py
+++ cmip6_extracting.py
@@ -13,14 +13,14 @@ import pandas as pd
 
 class CMIP6Extraction(object):
     def __init__(self):
-        self.input = r"./cmip6_outputs"
-        self.output = r"./cmip6_outputs/output"
+        self.input = r"/home/baris/Desktop/new/data/cmip6/models/tas_day_CNRM-ESM2-1_historical_r1i1p1f2_gr_1995_2004"
+        self.output = r"/home/baris/Desktop/new/data/cmip6/output"
         self.cmip6_nc_list = []
         self.cmip6_shp_list = []
         self.results_database = []
 
     def find_cmip6_netcdf(self):
-        for cmip6 in pathlib.Path(self.input).glob('**/*.nc'):
+        for cmip6 in pathlib.Path(self.input).glob('**/tas_day_CNRM-ESM2-1_historical_r1i1p1f2_gr_1995_2004.nc'):
             self.cmip6_nc_list.append(cmip6)
 
     def find_cmip6_grids(self):
@@ -29,31 +29,27 @@ class CMIP6Extraction(object):
 
     def extract_cmip6(self, cmip6_nc_path, cmip6_shp_path):
         self.results_database = []
-        cmip6_nc = xr.open_dataset(cmip6_nc_path)
-        affine = rio.open(cmip6_nc_path).transform
-        shp_df = gpd.read_file(cmip6_shp_path)
-        all_days = cmip6_nc['time'].values  # get the days of cmip6
-        # loop into each day and calculate statistics
-        for day in range(len(all_days)):
-            variable = cmip6_nc.variable_id
-            cmip6_da = cmip6_nc[variable][day, :, :]
-            cmip6_da = cmip6_da.values
-            cmip6_values = zonal_stats(shp_df.geometry, cmip6_da,
-                                     affine=affine, stats=['majority'])
-            current_date = all_days[day]
-            date = np.full(shape=len(cmip6_values), fill_value=current_date)
-            cmip6_values_pd = pd.DataFrame.from_records(cmip6_values)
-            cmip6_values_pd["date"] = date
-            self.results_database.append(cmip6_values_pd)
+        cmip6_name = '-'.join(os.path.split(current_cmip6_shp)[1].split('_')[1:-1])
 
-            # see progress in the console
-            print(f"Total Processed Day: {day + 1}/{len(all_days)}")
+        cmip6_nc = xr.open_dataset(cmip6_nc_path)
+        shp_df = gpd.read_file(cmip6_shp_path)
+        # loop into each day and calculate statistics
+        for i in range(len(shp_df)):
+            lon = shp_df['geometry'][i].x
+            lat = shp_df['geometry'][i].y
+            dataset = cmip6_nc.sel(lat=lat, lon=lon,method='nearest')
+            values = dataset.tas.to_numpy()
+            date = dataset.time.to_numpy()
+            id = shp_df['id'][i]
+            self.results_database.append(pd.DataFrame({
+                'cmip6_values': values,
+                'date':date,
+                'id':id
+            }))
+            print(f"Total grid processed: {i+1}/{len(shp_df)}")
         self.results_database = pd.concat(self.results_database)
-        current_grid_name = '-'.join(os.path.split(current_cmip6_shp)[1].split('_')[1:-1])
-        write_path = os.path.join(self.output, current_grid_name + '.csv')
-        new_column_names = ['cmip6_value', 'date']
-        self.results_database.set_axis(new_column_names, axis=1, inplace=True)
-        self.results_database.to_csv(path_or_buf=write_path, index_label='id')
+        self.results_database.to_csv(os.path.join(self.output, cmip6_name+'.csv'))
+
 
 
 if __name__ == '__main__':
